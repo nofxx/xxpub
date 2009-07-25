fc18a99060319b051c3e501ebff88883bd00a37f
Marcos Piccinini <x@nofxx.com>
2009-07-25 06:17:21 -0300

    rake rox

--- code/c/crashcourse/fundamental.c
+++ code/c/crashcourse/fundamental.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
-main()
-{
-  int x = 1, y = 5, z;
-  int xo = 045;
-  int xh = 0x8F;
-  
-  
-  float xx, yy = 23.6e-2;
-  double zz = 23.1L;
-  
-  char a;
-  char c = 'x';
-  
-  
-  x = y + 5;
-  xx = 23.344;
-  printf("%d %d %d\n", x, xo, yy);
-  x++;
-  y = ( x > 100) ? 10 : 20;
-  printf("x++:  x=%d, y=%d\n", x, y);
-  
-  primidor(2);
-  // int input;
-  // scanf("%d", &input);
-}
-
+/* methods before main! */
 void primidor(int algo)
 {
   int total;
   total = 100 * algo;
   printf("Total %d \n", total);
 }
+
+main()
+{
+  int x = 1, y = 5, z;
+  int xo = 045;
+  int xh = 0x8F;
+
+  float xx, yy = 23.6e-2;
+  double zz = 23.1L;
+
+  char a;
+  char c = 'x';
+
+  x = y + 5;
+  xx = 23.344;
+  printf("%d %d %d\n", x, xo, yy);
+  x++;
+  y = ( x > 100) ? 10 : 20;
+  printf("x++:  x=%d, y=%d\n", x, y);
+
+  primidor(2);
+  // int input;
+  // scanf("%d", &input);
+}
+
+
 
 // Table 1: Keywords (K & R, p. 192)
 // Data Type Declarations                 Control Flow Statements
--- code/c/crashcourse/xmath.c
+++ code/c/crashcourse/xmath.c
@@ -1,12 +1,12 @@
 #include <math.h>
+#include <stdio.h>
 
 main()
 {  int i;
-  
+
   printf(" Numb:  Square Root of Number\n\n");
 
   for (i=0; i<=360; ++i)
-         printf("N  %d => %d \n",i, sqrt((double) i));
-  
-  
+  printf("N  %d => %d \n",i, sqrt((double) i));
+
 }
