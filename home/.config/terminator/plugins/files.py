3fcff8d99eaa36301d6e8871dddc7f11fcefe2e6
Marcos Piccinini <x@nofxx.com>
2014-11-24 12:51:45 -0200

    terminator uri click open relatives paths

--- home/.config/terminator/plugins/files.py
+++ home/.config/terminator/plugins/files.py
@@ -1,12 +1,13 @@
 # Clickable Files by nofxx
 """files.py - Clickable Files URIs open in Emacs"""
-import re
+import re, os, subprocess, inspect
 import terminatorlib.plugin as plugin
-import subprocess
+# from terminatorlib import plugin
+# from terminatorlib import config
 
 # Every plugin you want Terminator to load *must* be listed in 'AVAILABLE'
 AVAILABLE = ['FileURLHandler']
-
+ 
 class FileURLHandler(plugin.URLHandler):
     capabilities = ['url_handler']
     handler_name = 'file_path'
@@ -14,6 +15,19 @@ class FileURLHandler(plugin.URLHandler):
     # Match any non-space string starting with an alphanumericm or a slash
     # and ending with a colon followed by a number
     match = r'[a-zA-Z0-9\.\/][^ ]+:[[:digit:]]+|[a-zA-Z0-9\/][^ ]+\([[:digit:]]+\)'
+
+    # Get current terminator path
+    # Thanks to https://github.com/mchelem/terminator-grep-plugin/
+    def get_cwd(self):
+        """ Return current working directory. """
+        # HACK: Because the current working directory is not available to plugins,
+        # we need to use the inspect module to climb up the stack to the Terminal
+        # object and call get_cwd() from there.
+        for frameinfo in inspect.stack():
+            frameobj = frameinfo[0].f_locals.get('self')
+            if frameobj and frameobj.__class__.__name__ == 'Terminal':
+                return frameobj.get_cwd()
+        return None
 
     def callback(self, url):
         # Change file(N) to file:N
@@ -28,6 +42,8 @@ class FileURLHandler(plugin.URLHandler):
         else:
           fname, line = params
           opts = "+" + line
+        if(fname[0] != '/'):
+          fname = os.path.join(self.get_cwd(), fname)
         print "Opening..." + fname + "#" + opts
         subprocess.call(["emacsclient", "-n", opts, fname])
         return "some_nice_shining_string"
